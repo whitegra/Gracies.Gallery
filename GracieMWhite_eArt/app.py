3c9b245ecfd28097983a2741517ab6bd0ca39833
Gracie White <139240618+whitegra@users.noreply.github.com>
2025-01-10 18:50:46 -0500

    Update app.py

--- GracieMWhite_eArt/app.py
+++ GracieMWhite_eArt/app.py
@@ -16,5 +16,8 @@ def artwork(artwork_id):
     artwork = ARTWORKS[artwork_id]
     return render_template('artwork.html', artwork=artwork)
 
+import os
+
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 8080))  # Use the PORT Replit provides or default to 8080
+    app.run(host='0.0.0.0', port=port)
